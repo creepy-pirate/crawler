5908b7bbfa61e5ea35f68228dd108de840ceeb5f
Deepak Singh <dv72276@gmail.com>
2023-05-23 04:08:56 +0530

    problems data not going in json and csv

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-import requests
 from bs4 import BeautifulSoup
 import json
 import csv
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 import re
+import requests
 
 app = Flask(__name__)
 
 # Function to scrape Google search results
-import re
-
-# Function to scrape Google search results
-def scrape_google_results(query, num_results):
+def scrape_google_results(query, num_results, convert_links):
     results = []
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
@@ -27,22 +24,17 @@ def scrape_google_results(query, num_results):
         for result in search_results:
             link = result.get('href')
             if link:
-                if link.startswith('/url?q='):
-                    link = link[7:]
-                    if 'youtube.com/channel/' in link:
-                        results.append(link)
-                elif link.startswith('https://www.youtube.com/watch?v='):
-                    channel_url = get_channel_url(link)
-                    if channel_url:
-                        results.append(channel_url)
+                if '/channel' in link or '/@' in link or '/c' in link:
+                    results.append('https://' + str(link))
+                if '/watch?v' in link:
+                    if convert_links:
+                        channel_url = get_channel_url(link)
+                        if channel_url:
+                            results.append(channel_url)
+                    else:
+                        results.append('https://' + str(link))
 
     return results[:num_results]
-
-
-
-
-
-
 
 # Function to save results as JSON
 def save_as_json(results, file_name):
@@ -63,12 +55,19 @@ def index():
         site_name = "youtube.com"
         query = request.form.get('query')
         num_results = int(request.form.get('num_results'))
+        convert_links = request.form.get('convert_links')
+        if convert_links == "on":
+            convert_links = True
+        else:
+            convert_links = False
 
-        
+        if not query or not num_results:
+            return render_template('index.html', message='Please provide query and number of results.')
+
         formatted_query = f'site:{site_name}+{query}'
 
         # Scrape Google search results
-        results = scrape_google_results(formatted_query, num_results)
+        results = scrape_google_results(formatted_query, num_results, convert_links)
 
         # Save results in JSON format
         save_as_json(results, 'youtube_links.json')
@@ -76,14 +75,46 @@ def index():
         # Save results in CSV format
         save_as_csv(results, 'youtube_links.csv')
 
-        return jsonify({'message': 'Scraping completed. Results saved as "youtube_links.json" and "youtube_links.csv".'})
+        return redirect('/download')
 
     return render_template('index.html')
 
 
+@app.route('/download')
+def download():
+    return render_template('downloadPage.html', message='Scraping completed.')
 
 
 
+def get_channel_url(video_url):
+    # Extract the channel ID from the video URL
+    channel_id = None
+    if 'youtube.com/watch' in video_url:
+        params = video_url.split('?')[1]
+        for param in params.split('&'):
+            if param.startswith('v='):
+                video_id = param.split('=')[1]
+                break
+        if video_id:
+            api_url = f"https://www.googleapis.com/youtube/v3/videos?id={video_id}&key=YOUR_API_KEY&part=snippet"
+            response = requests.get(api_url)
+            video_data = response.json()
+            if 'items' in video_data:
+                channel_id = video_data['items'][0]['snippet']['channelId']
+    elif 'youtu.be/' in video_url:
+        video_id = video_url.split('/')[-1]
+        api_url = f"https://www.googleapis.com/youtube/v3/videos?id={video_id}&key=YOUR_API_KEY&part=snippet"
+        response = requests.get(api_url)
+        video_data = response.json()
+        if 'items' in video_data:
+            channel_id = video_data['items'][0]['snippet']['channelId']
+
+    # Construct the channel URL
+    if channel_id:
+        channel_url = f"https://www.youtube.com/channel/{channel_id}"
+        return channel_url
+    else:
+        return "Could not be converted"
 
 
 import requests
